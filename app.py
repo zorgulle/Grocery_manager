05fe5b34c5915aba6bba3a8e9be96ce896d35d90
Adrien <winy95battle@gmail.com>
2017-10-30 21:45:15 +0100

    Add sqlite db connection

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
+import sqlite3
 from flask import Flask
+from flask import g
+
+DATABASE = './database.db'
 
 app = Flask(__name__)
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+
+    db.row_factory = sqlite3.Row
+
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db = db.close()
+
 @app.route('/')
 def index():
-    return 'Index page'
+    cur = get_db().execute("SELECT nom FROM test")
+    result = cur.fetchall()
+    cur.close()
+    for r in result:
+        return r['nom']
 
 
 if __name__ == '__main__':
